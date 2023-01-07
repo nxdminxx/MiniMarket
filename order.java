f95cce2a977bead8cecf2d6c4735cda13cd71041
nurizzatisaidi <nurizzatisaidi2@gmail.com>
2023-01-07 23:48:55 +0800

    zati first push

---
+++ order.java
@@ -0,0 +1,232 @@
+package fastfoodjava;
+public class order {
+    public String entreeA = null,entreeB = null,entreeC = null,
+            entreeD = null,entreeE = null;
+    public String sideDishA =null,sideDishB =null,sideDishC =null,
+            sideDishD =null,sideDishE =null;
+    public String drinkA=null,drinkB=null,drinkC =null,
+            drinkD =null,drinkE =null;
+    public double entreePriceA,entreePriceB,entreePriceC,entreePriceD,
+            entreePriceE;
+    public double sideDishPriceA,sideDishPriceB,sideDishPriceC,sideDishPriceD,
+            sideDishPriceE;
+    public double drinkPriceA,drinkPriceB,drinkPriceC,drinkPriceD,
+            drinkPriceE;
+
+//entreeA
+	public String getEntreeA(){
+		return entreeA;
+	}
+	public void setEntreeA(String entreeA){
+			this.entreeA = entreeA;
+	}
+
+//entreePriceA
+	public double getEntreePriceA(){
+		return entreePriceA;
+	}
+	public void setEntreePriceA(double entreePriceA){
+		this.entreePriceA = entreePriceA;
+	}
+//entreeB
+	public String getEntreeB(){
+		return entreeB;
+	}
+	public void setEntreeB(String entreeB){
+			this.entreeB = entreeB;
+	}
+
+//entreePriceB
+	public double getEntreePriceB(){
+		return entreePriceB;
+	}
+	public void setEntreePriceB(double entreePriceB){
+		this.entreePriceB = entreePriceB;
+	}
+//entreeC
+	public String getEntreeC(){
+		return entreeC;
+	}
+	public void setEntreeC(String entreeC){
+			this.entreeC= entreeC;
+	}
+
+//entreePriceC
+	public double getEntreePriceC(){
+		return entreePriceC;
+	}
+	public void setEntreePriceC(double entreePriceC){
+		this.entreePriceC = entreePriceC;
+	}
+//entreeD
+	public String getEntreeD(){
+		return entreeD;
+	}
+	public void setEntreeD(String entreeD){
+			this.entreeD = entreeD;
+	}
+
+//entreePriceD
+	public double getEntreePriceD(){
+		return entreePriceD;
+	}
+	public void setEntreePriceD(double entreePriceD){
+		this.entreePriceD = entreePriceD;
+	}
+//entreeE
+	public String getEntreeE(){
+		return entreeE;
+	}
+	public void setEntreeE(String entreeE){
+			this.entreeE = entreeE;
+	}
+
+//entreePriceE
+	public double getEntreePriceE(){
+		return entreePriceE;
+	}
+	public void setEntreePriceE(double entreePriceE){
+		this.entreePriceE = entreePriceE;
+	}
+        
+//SideDishA
+	public String getSideDishA(){
+		return sideDishA;
+	}
+	public void setSideDishA(String sideDishA){
+			this.sideDishA = sideDishA;
+	}
+//sideDishPriceA
+	public double getSideDishPriceA(){
+		return sideDishPriceA;
+	}
+	public void setSideDishPriceA(double sideDishPriceA){
+		this.sideDishPriceA = sideDishPriceA;
+	}
+//SideDishB
+	public String getSideDishB(){
+		return sideDishB;
+	}
+	public void setSideDishB(String sideDishB){
+			this.sideDishB = sideDishB;
+	}
+//sideDishPriceB
+	public double getSideDishPriceB(){
+		return sideDishPriceB;
+	}
+	public void setSideDishPriceB(double sideDishPriceB){
+		this.sideDishPriceB = sideDishPriceB;
+	}
+//SideDishC
+	public String getSideDishC(){
+		return sideDishC;
+	}
+	public void setSideDishC(String sideDishC){
+			this.sideDishC = sideDishC;
+	}
+//sideDishPriceC
+	public double getSideDishPriceC(){
+		return sideDishPriceC;
+	}
+	public void setSideDishPriceC(double sideDishPriceC){
+		this.sideDishPriceC = sideDishPriceC;
+	}
+ //SideDishD
+	public String getSideDishD(){
+		return sideDishD;
+	}
+	public void setSideDishD(String sideDishD){
+			this.sideDishD = sideDishD;
+	}
+//sideDishPriceD
+	public double getSideDishPriceD(){
+		return sideDishPriceD;
+	}
+	public void setSideDishPriceD(double sideDishPriceD){
+		this.sideDishPriceD = sideDishPriceD;
+	}  
+  //SideDishE
+	public String getSideDishE(){
+		return sideDishE;
+	}
+	public void setSideDishE(String sideDishE){
+			this.sideDishE = sideDishE;
+	}
+//sideDishPriceD
+	public double getSideDishPriceE(){
+		return sideDishPriceE;
+	}
+	public void setSideDishPriceE(double sideDishPriceE){
+		this.sideDishPriceE = sideDishPriceE;
+	}
+//drinkA
+	public String getDrinkA(){
+		return drinkA;
+	}
+	public void setDrinkA(String drinkA){
+			this.drinkA = drinkA;
+	}
+//drinkPriceA
+	public double getDrinkPriceA(){
+		return drinkPriceA;
+	}
+	public void setDrinkPriceA(double drinkPriceA){
+		this.drinkPriceA= drinkPriceA;
+	}
+//drinkB
+	public String getDrinkB(){
+		return drinkB;
+	}
+	public void setDrinkB(String drinkB){
+			this.drinkB = drinkB;
+	}
+//drinkPriceB
+	public double getDrinkPriceB(){
+		return drinkPriceB;
+	}
+	public void setDrinkPriceB(double drinkPriceB){
+		this.drinkPriceB = drinkPriceB;
+	}
+ //drinkC
+	public String getDrinkC(){
+		return drinkA;
+	}
+	public void setDrinkC(String drinkC){
+			this.drinkC = drinkC;
+	}
+//drinkPriceC
+	public double getDrinkPriceC(){
+		return drinkPriceC;
+	}
+	public void setDrinkPriceC(double drinkPriceC){
+		this.drinkPriceC = drinkPriceC;
+	}
+ //drinkD
+	public String getDrinkD(){
+		return drinkD;
+	}
+	public void setDrinkD(String drinkD){
+			this.drinkD = drinkD;
+	}
+//drinkPriceD
+	public double getDrinkPriceD(){
+		return drinkPriceD;
+	}
+	public void setDrinkPriceD(double drinkPriceD){
+		this.drinkPriceD = drinkPriceD;
+	}  
+ //drinkE
+	public String getDrinkE(){
+		return drinkE;
+	}
+	public void setDrinkE(String drinkE){
+			this.drinkE = drinkE;
+	}
+//drinkPriceE
+	public double getDrinkPriceE(){
+		return drinkPriceE;
+	}
+	public void setDrinkPriceE(double drinkPriceE){
+		this.drinkPriceE = drinkPriceE;
+	}             
+}
