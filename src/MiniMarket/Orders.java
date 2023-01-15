d3239dd91235bc1b8931e472ea354d1f3f132641
Stardeous <78629692+nxdminxx@users.noreply.github.com>
2023-01-15 18:45:27 +0800

    update order file

--- src/MiniMarket/Orders.java
+++ src/MiniMarket/Orders.java
@@ -1,8 +1,45 @@
 package MiniMarket;
+import java.util.ArrayList;
 
 public class Orders {
+
+    private String orderID;
+    Customer customer;
+    ArrayList<Product> Product;
     
     Orders(){
 
+    }
+
+    Orders(Customer customer,ArrayList<Product> product){
+        
+    }
+
+    public String getOrderID(){
+
+        return orderID;
+
+    }
+
+    public Customer getCustomer(){
+
+        return customer;
+        
+    }
+
+    public ArrayList<MiniMarket.Product> getProduct(){
+
+        return Product;
+
+    }
+
+    public double calcTotalPrice(){
+
+        return 0;
+
+    }
+
+    public void setOrderID(String orderID){
+        
     }
 }
