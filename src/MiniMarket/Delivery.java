2d9068641575d1b1fa38ce24c763ae80ff217644
noorsafiza <safizaismail27@gmail.com>
2023-01-21 00:53:44 +0800

    update delivery file

--- src/MiniMarket/Delivery.java
+++ src/MiniMarket/Delivery.java
@@ -1,8 +1,30 @@
 package MiniMarket;
+import java.util.ArrayList;
 
-public class Delivery {
-    
-    Delivery(){
+public class Delivery{
+
+    public static void main (String[]args) {
+
+    private String deliveryAddress; 
+
+    public Delivery(Customer cust, prod ArrayList<Product>, String deliveryAddress){
+
+        this.deliveryAddress = deliveryAdrress; 
+    }
+
+    public double getDeliveryAddress(){
+        return getDeliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliverAddress){
+        deliveryAddress = deliveryAddress;
+    }
+
+    public double calcDeliveryCharge(){
+        return calcDeliveryCharge;
+    }
 
     }
+
+    
 }
