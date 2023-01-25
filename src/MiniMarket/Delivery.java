72dc1710efcb378c3fa18a14c4d61a77d3c1eea0
nurizzatisaidi <nurizzatisaidi2@gmail.com>
2023-01-25 21:33:03 +0800

    Banyak gak la dah diubah

--- src/MiniMarket/Delivery.java
+++ src/MiniMarket/Delivery.java
@@ -1,8 +1,18 @@
 package MiniMarket;
 
 public class Delivery {
-    
+    String address;
+    Orders order;
     Delivery(){
-
+        address = "";
+    }
+    Delivery(String address){
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address){
+        address = address;
     }
 }
